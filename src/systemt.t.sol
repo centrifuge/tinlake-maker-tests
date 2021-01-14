bdc6259974d65032183d144284ac6c7028100987
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2021-01-14 14:23:01 +0100

    test framework

---
+++ src/systemt.t.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.15;
+
+import "ds-test/test.sol";
+
+import "./TinlakeMkrTest.sol";
+
+contract TinlakeMkrTest is DSTest {
+
+    function setUp() public {
+
+    }
+
+    function test_basic_sanity() public {
+
+    }
+}
