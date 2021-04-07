e9504d6714ad2e51f226eea05ba48ef68eac2b2b
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2021-04-07 15:57:30 +0200

    fix: test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -2,8 +2,8 @@
 
 set -e
 # workaround for same solidity compiler version
-# upgrade solidity 0.5.12 files to 0.5.15
-find ./ -type f -name "*.sol" -print0 | xargs -0 sed -i '' -e 's/0.5.12/0.5.15/g'
+# change compiler solidity version from 0.5.12 to 0.5.15
+egrep -lRZ '0.5.12' . | xargs -0 -l sed -i -e 's/0.5.12/0.5.15/g'
 
 dapp --use solc:0.5.15 test
 
